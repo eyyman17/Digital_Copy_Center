e4207d43043b277d0f661ef6718e8a9f702c83d1
eyyman17 <aymane.lamarti.8@gmail.com>
2024-10-14 03:42:43 +0100

    login page by default

--- digital_copy_center/urls.py
+++ digital_copy_center/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('agent.urls')),
-    path('', include('administration.urls')),
-    path('', include('professors.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=True)),
 ]
