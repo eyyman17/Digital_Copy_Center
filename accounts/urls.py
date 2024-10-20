494c493f2f1a07888732551a312c286d57a63e3f
eyyman17 <aymane.lamarti.8@gmail.com>
2024-10-20 14:44:24 +0100

    resetting password

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,10 +15,5 @@ urlpatterns = [
 
     # Password reset URLs
     path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-                template_name='reset/password_reset_confirm.html'
-                ), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-                template_name='reset/password_reset_complete.html'
-                ), name='password_reset_complete'),
+    path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -15,10 +15,9 @@ def redirect_user(request):
     else:
         return redirect('login')  # Fallback
     
-from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 from django.contrib import messages
-from django.urls import reverse_lazy
-
+from django.urls import reverse_lazy, reverse
 
 class CustomPasswordResetView(PasswordResetView):
     template_name = 'reset/password_reset_form.html' 
@@ -29,3 +28,13 @@ class CustomPasswordResetView(PasswordResetView):
     def form_valid(self, form):
         messages.success(self.request, "Le lien de réinitialisation a été envoyé à votre adresse email.")
         return super().form_valid(form)
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name='reset/password_reset_confirm.html'
+    success_url = reverse_lazy('password_reset_confirm')
+
+    def form_valid(self, form):
+        form.save()  # Ensure password is saved
+        messages.success(self.request, "Votre mot de passe a été réinitialisé avec succès! Vous pouvez maintenant vous connecter.")
+        return self.render_to_response(self.get_context_data(form=form))
