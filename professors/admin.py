f3045b4544c505a49a264f6754793b8cded1bf3c
eyyman17 <aymane.lamarti.8@gmail.com>
2025-01-07 19:18:24 +0100

    django admin panel

--- professors/admin.py
+++ professors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('professeur', 'document_file', 'date', 'validation_impression', 'n_copies', 'format', 'couleur')
+    search_fields = ('professeur__username', 'professeur__email', 'document_file')
+    list_filter = ('validation_impression', 'format', 'couleur', 'date')
