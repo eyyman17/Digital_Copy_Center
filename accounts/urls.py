a732aa64c2cee5da9e0e03ee6818d6dee5d78918
eyyman17 <aymane.lamarti.8@gmail.com>
2025-01-21 02:36:59 +0100

    return

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('csrf/', views.get_csrf_token, name='get_csrf_token'),
-    path('login/', views.login_api, name='login_api'),
-    path('logout/', views.logout_api, name='logout_api'),
+    path('login/', views.login_api, name='login'),
+    path('logout/', views.logout_api, name='logout'),
     path('password_reset/', views.password_reset_api, name='password_reset_api'),
     path('password_reset_confirm/<uidb64>/<token>/', views.password_reset_confirm_api, name='password_reset_confirm_api'),
 ]
