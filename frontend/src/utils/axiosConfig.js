157ea5d4178ca594cb1ee4755ec4e7ed509838e7
eyyman17 <aymane.lamarti.8@gmail.com>
2025-02-19 18:27:14 +0100

    axios2

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://digital-copy-center-backend.onrender.com';
 axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
